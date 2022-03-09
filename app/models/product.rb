7572c4f8b7827a7110d31c80c1c20e0240b9acae
Vishal Verma <vishal@genetico.in>
2022-03-09 20:13:51 +0530

    add new schema

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,5 @@ class Product < ApplicationRecord
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  belongs_to :user
   has_and_belongs_to_many :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+chris = User.create!(name: 'chris', email: 'chris66666@test.com', password: 'chris123')
+category1 = Category.create!(name: "Bottle")
+product1 = Product.create!(name:"Water bottle",category_id:category1.id,price: 500, description: "Rolex Water bottle from switz alps",quantity: 10);
+order1 = Order.create!(product_id: product1.id,quantity:2,user_id:chris.id);
