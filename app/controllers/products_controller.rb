c1430e475d4a5717f4d5b5872410c642b66ba341
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-10 16:21:12 +0000

    Add create and delete features for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,45 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+        render json: {
+            products: @products
+        }
+    end
+    
+    def create
+        @category = Category.find_by_id(product_params[:category_id])
+        @product = @category.products.new(product_params.except(:category_id))
+        if @product.save
+            render json: {
+                status: :created,
+                product: @product
+            }
+        else
+            render json: {
+                status: 500,
+                errors: @product.errors.full_messages
+            }
+        end
+    end
+
+    def destroy
+        @product = Product.find_by_id(params[:id])
+        if @product.delete
+            render json: {
+                status: :destroyed
+            }
+        else
+            render json: {
+                status: 500,
+                errors: @product.errors.full_messages
+            }
+        end
+    end
+
+    private
+
+    def product_params
+        params.permit(:name, :description, :img, :price,
+        :quantity, :category_id)
+    end
 end
