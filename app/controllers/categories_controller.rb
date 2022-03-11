7413f7d88010fd3f6f058c5a103acc0d831a3d4e
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-11 17:53:01 +0000

    Retrive and create categories features

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,29 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        render json: {
+            categories: @categories
+        }
+    end
+
+    def create
+        @category = Category.new(categories_params)
+        if @category.save
+            render json: {
+                status: :created,
+                category: @category
+            }
+        else
+            render json: {
+                status: 500,
+                errors: @category.errors.full_messages
+            }
+        end
+    end
+
+    private
+
+    def categories_params
+        params.permit(:name)
+    end
 end
