409196d506ade181bedc6e606849993106e454f6
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-11 17:53:38 +0000

    Fix rubocop offenses

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,29 @@
 class CategoriesController < ApplicationController
-    def index
-        @categories = Category.all
-        render json: {
-            categories: @categories
-        }
-    end
+  def index
+    @categories = Category.all
+    render json: {
+      categories: @categories
+    }
+  end
 
-    def create
-        @category = Category.new(categories_params)
-        if @category.save
-            render json: {
-                status: :created,
-                category: @category
-            }
-        else
-            render json: {
-                status: 500,
-                errors: @category.errors.full_messages
-            }
-        end
+  def create
+    @category = Category.new(categories_params)
+    if @category.save
+      render json: {
+        status: :created,
+        category: @category
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @category.errors.full_messages
+      }
     end
+  end
 
-    private
+  private
 
-    def categories_params
-        params.permit(:name)
-    end
+  def categories_params
+    params.permit(:name)
+  end
 end
